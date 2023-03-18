12d12383c41aa23dca3c839e118b8a6d6588802c
Aly Muhammad Aly <deswigoaly@gmail.com>
2023-03-18 21:06:47 +0200

    first project

---
+++ DigitalClock/digitalClock.py
@@ -0,0 +1,26 @@
+import tkinter as Tk
+import time
+
+def update_clock():
+	date = time.strftime("%d/%m/%Y")
+	current_time = time.strftime("%H:%M:%S")
+	date_time_text = date + " " + current_time
+
+	clock_label.config(text=date_time_text)
+	clock_label.after(1000, update_clock)
+	
+
+def main():
+	global window, clock_label
+
+	window = Tk.Tk()
+	window.title("Digital Clock")
+
+	clock_label = Tk.Label(window, font="Helventica 72 bold")
+	clock_label.pack()
+
+	update_clock()
+	window.mainloop()
+
+if __name__ == '__main__':
+	main()
